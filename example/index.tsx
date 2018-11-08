251a6997ec705525191df728de02ba44a62cbb80
Joseph Weidinger <jsphweid@gmail.com>
2018-11-07 21:55:27 -0600

    basic example

---
+++ example/index.tsx
@@ -0,0 +1,59 @@
+import * as React from 'react'
+import * as ReactDOM from 'react-dom'
+import * as RecordingAudioEngine from '../src'
+
+interface ExampleState {
+  recordings: RecordingAudioEngine.MonoRecording[]
+}
+
+class Example extends React.Component<any, ExampleState> {
+  constructor(props: any) {
+    super(props)
+    this.state = {
+      recordings: []
+    }
+  }
+
+  private handleStopRecording() {
+    RecordingAudioEngine.RecordingEngine.stopRecording()
+    const latestRecording = RecordingAudioEngine.RecordingEngine.latestRecording
+    if (latestRecording) {
+      this.setState({
+        recordings: [...this.state.recordings, latestRecording]
+      })
+    }
+  }
+
+  private renderStartStop() {
+    const { startRecording } = RecordingAudioEngine.RecordingEngine
+    return (
+      <div>
+        <button onClick={() => startRecording()}>start recording</button>
+        <button onClick={() => this.handleStopRecording()}>
+          stop recording
+        </button>
+      </div>
+    )
+  }
+
+  private renderRecordings() {
+    if (!this.state.recordings) return null
+    const lis = this.state.recordings.map((recording, i) => (
+      <li key={i} onClick={() => console.log(recording)}>
+        Recording {i + 1}
+      </li>
+    ))
+    return <ul>{lis}</ul>
+  }
+
+  public render() {
+    return (
+      <div>
+        {this.renderStartStop()}
+        {this.renderRecordings()}
+      </div>
+    )
+  }
+}
+
+ReactDOM.render(<Example />, document.getElementById('example'))
---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  devtool: 'inline-source-map',
+  resolve: {
+    extensions: ['.ts', 'tsx', '.js']
+  },
+  entry: './example/index.tsx',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        loader: 'awesome-typescript-loader?configFileName=tsconfig.example.json'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.join(__dirname, 'example/index.ejs')
+    })
+  ]
+}
